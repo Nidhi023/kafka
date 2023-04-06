f51d787d80eaa2b56182954c1612ac32e66e5661
nidhi <nidhi.ar@accionlabs.com>
2023-04-06 15:48:04 +0530

    First commit

---
+++ src/main/java/com/kafka/demo/User.java
@@ -0,0 +1,39 @@
+package com.kafka.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+	private int id;	
+	User(int id,String name,String[] address) {
+		this.id=id;
+		this.name=name;
+		this.address=address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getAddress() {
+		return address;
+	}
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+	private String name;
+	private String[] address;
+
+}
